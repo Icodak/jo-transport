44a0b4ba845a073ba4d91d76dbf4ae2eba40a610
Icodak <24356594+Icodak@users.noreply.github.com>
2023-07-29 12:54:27 +0200

    [feat] Add header button & hint

--- jo-transport-app/src/main/java/fr/isep/jotransportapp/components/MainController.java
+++ jo-transport-app/src/main/java/fr/isep/jotransportapp/components/MainController.java
@@ -2,12 +2,18 @@ package fr.isep.jotransportapp.components;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 
 public class MainController implements Initializable {
+    @FXML
+    public Button addStep;
+    @FXML
+    public Label hint;
     @FXML
     private TitleTextField departure;
 
@@ -20,7 +26,12 @@ public class MainController implements Initializable {
         departure.setTitle("Départ");
         departure.getTextProperty().addListener(System.out::println);
 
+        addStep.setText("+ Ajouter une étape");
+        addStep.setOnAction(e -> System.out.println("PAF!"));
+
         arrival.setTitle("Arrivée");
         arrival.getTextProperty().addListener(System.out::println);
+
+        hint.setText("Saisissez au moins un départ et une arrivée");
     }
 }
